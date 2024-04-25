25115be4159314ae62a7dccf3216ec3c93f6f433
Axel Kaliff <axel.kaliff@protonmail.com>
2024-04-25 18:31:42 +0200

    nixvim

--- nixvim/config/default.nix
+++ nixvim/config/default.nix
@@ -3,30 +3,4 @@
   imports = [
     ./bufferline.nix
   ];
-
-  colorschemes.gruvbox.enable = true;
-
-  plugins = {
-  	lualine.enable = true;
-	telescope.enable = true;
-	oil.enable = true;
-	treesitter.enable = true;
-	luasnip.enable = true;
-	harpoon.enable = true;
-  };
-
-  plugins.lsp = {
-  	enable = true;
-	servers = {
-	lua-ls.enable = true;
-	rust-analyzer.enable = true;
-	};
-	};
-
-plugins.cmp = {
-	enable = true;
-	autoEnableSources = true;
-	};
-
-
 }
--- nixvim/nixvim.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-{
-  programs.nixvim = {
-    enable = true;
-  };
-
-}
