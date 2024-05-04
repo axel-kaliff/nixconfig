990f255c19bf3d57988e34b127738016c82461e9
Axel Kaliff <axel.kaliff@protonmail.com>
2024-05-04 13:43:15 +0200

    enabled autoupdates

--- configuration.nix
+++ configuration.nix
@@ -46,6 +46,18 @@ in
 		
 	];
 
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+    "--update-input"
+    "nixpkgs"
+    "-L"
+    ];
+    dates = "09:00";
+    randomizedDelaySec = "45min";
+  };
+
 
 
   # Bootloader.
