19ed56b6d75464e6a57487c4bb88dedcf10742c1
Axel Kaliff <axel.kaliff@protonmail.com>
2024-05-24 08:22:37 +0200

    broke out xremap

--- flake.nix
+++ flake.nix
@@ -28,36 +28,19 @@
       specialArgs = {inherit inputs;};
 
     modules = [
-      
-    inputs.xremap-flake.nixosModules.default
+            inputs.xremap-flake.nixosModules.default
+            ./xremap.nix
             {
-                nix.settings = {
+              nix.settings = {
                 substituters = [ "https://cosmic.cachix.org/" ];
                 trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-                };
-    
-   services.xremap.config.modmap = [
-                {
-                name = "Global";
-                remap = { 
-    
-                    "Alt_L" = "Win_L"; 
-                    "Win_L" = "Alt_L"; 
-                    "CapsLock" = "LeftCtrl"; 
-    
-               }; 
-                }
-            ];
-    
-       services.xremap.config.keymap = [
-                {
-                    name = "navigation";
-                    remap = { 
-        "C-k" = "PAGEUP"; 
-        "C-j" = "PAGEDOWN"; 
-        };
-                }
-                ];
+              };
+            }
+            ./configuration.nix
+            inputs.home-manager.nixosModules.default
+            # inputs.nixvim.nixosModules.nixvim
+            # ./nixvim/nixvim.nix
+          ];
             }
             nixos-cosmic.nixosModules.default
             ./configuration.nix
---
+++ xremap.nix
@@ -0,0 +1,24 @@
+{ lib, ... }:
+{
+  services.xremap.config.modmap = [
+    {
+      name = "Global";
+      remap = {
+        "Alt_L" = "Win_L";
+        "Win_L" = "Alt_L";
+        "CapsLock" = "LeftCtrl";
+      };
+    }
+  ];
+
+  services.xremap.config.keymap = [
+    {
+      name = "navigation";
+      remap = {
+        "C-k" = "PAGEUP";
+        "C-j" = "PAGEDOWN";
+      };
+    }
+  ];
+}
+
