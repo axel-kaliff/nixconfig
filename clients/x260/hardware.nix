1e041400c4ecc989ffa1ffe4c8c292237118e1c7
Axel Kaliff <axel.kaliff@protonmail.com>
2024-06-12 16:58:14 +0200

    x260 fix

--- clients/x260/hardware.nix
+++ clients/x260/hardware.nix
@@ -8,6 +8,5 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-f5fec819-00a0-497e-8382-663b5f90f6c1".device = "/dev/disk/by-uuid/f5fec819-00a0-497e-8382-663b5f90f6c1";
 }
 
