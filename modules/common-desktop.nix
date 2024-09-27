b6dcb0b24ee691911dff0b3df91c8c2168bfaf14
Axel Kaliff <axel.kaliff@protonmail.com>
2024-09-27 09:39:46 +0200

    moved gnome extensions to gnome config

--- modules/common-desktop.nix
+++ modules/common-desktop.nix
@@ -16,20 +16,13 @@
   services.flatpak.enable = true;
 
   environment.systemPackages = with pkgs; [
-    gnome-tweaks
     wofi
-    gnomeExtensions.pop-shell
-    gnomeExtensions.caffeine
-    gnomeExtensions.tray-icons-reloaded
-    gnomeExtensions.blur-my-shell
+
     nautilus
-    #power-profiles-daemon
-    gnomeExtensions.gnome-40-ui-improvements
     flatpak-builder
 
-
     # add system76 scheduler? how would I benchmark?
-    #linuxKernel.packages.linux_zen.system76-power
+    # linuxKernel.packages.linux_zen.system76-power
     # linuxKernel.packages.linux_zen.system76-scheduler
 
   ];
--- modules/gnome-desktop.nix
+++ modules/gnome-desktop.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ...}:
 
 {
   imports = [
@@ -7,5 +7,16 @@
 
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [ 
+    gnome-tweaks
+    gnomeExtensions.appindicator
+    gnomeExtensions.pop-shell
+    gnomeExtensions.caffeine
+    gnomeExtensions.tray-icons-reloaded
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.gnome-40-ui-improvements
+  ];
+
 }
 
