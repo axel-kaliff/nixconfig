86c743970fa894db85ecfed0117197ff73ec356c
Axel Kaliff <axel.kaliff@protonmail.com>
2024-10-02 20:13:05 +0200

    nvim to lia

--- packages/neovim.nix
+++ packages/neovim.nix
@@ -1,73 +1,9 @@
-{pkgs,...}:
+{lib, ...}:
 {
-  programs.nixvim = {
+  programs.neovim = {
     enable = true;
 
-    # Theme
-    colorschemes.tokyonight.enable = true;
+    extraLuaConfig = "${builtins.readFile ./nvim/init.lua}";
 
-    # Settings
-    opts = {
-      expandtab = true;
-      shiftwidth = 2;
-      smartindent = true;
-      tabstop = 2;
-      number = true;
-      #clipboard = "wl-clipboard";
-    };
-
-    # Keymaps
-    globals = {
-      mapleader = " ";
-    };
-
-      extraPlugins = with pkgs.vimPlugins; [
-      vim-nix
-      neo-tree-nvim
-      ];
-
-    plugins = {
-
-
-      # UI
-      lualine.enable = true;
-      bufferline.enable = true;
-      treesitter.enable = true;
-      which-key = {
-        enable = true;
-      };
-      
-      telescope = {
-        enable = true;
-        settings.keymaps = {
-          "<leader>ff" = {
-            desc = "file finder";
-            action = "find_files";
-          };
-          "<leader>fg" = {
-            desc = "find via grep";
-            action = "live_grep";
-          };
-        };
-        extensions = {
-          file-browser.enable = true;
-        };
-      };
-
-      # Dev
-      lsp = {
-        enable = true;
-        servers = {
-          hls.enable = true;
-          marksman.enable = true;
-          nil-ls.enable = true;
-          rust-analyzer = {
-            enable = true;
-            installCargo = false;
-            installRustc = false;
-          };
-        };
-      };
-    };
   };
 }
